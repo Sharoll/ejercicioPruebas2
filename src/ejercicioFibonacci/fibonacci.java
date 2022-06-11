d25bbb632dfb4c39704795a2800f78c0d2ef3157
Sharoll <syuliannaaraujo@unicesar.edu.co>
2022-06-11 16:10:58 -0500

    Escribir implementacion y ejecutar pruebas automatizadas

--- src/ejercicioFibonacci/fibonacci.java
+++ src/ejercicioFibonacci/fibonacci.java
@@ -2,8 +2,20 @@ package ejercicioFibonacci;
 
 public class fibonacci {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static int serieFibonacci(int valor) {
+		
+		 int a = 0;
+         int b = 1;
+
+         for (int i = 0; i < valor; i++)
+         {
+            int auxiliar = a;
+             a = b;
+             b = auxiliar + a;
+             
+         }
+
+         return a;
 
 	}
 
--- src/ejercicioFibonacci/testFibonacci.java
+++ src/ejercicioFibonacci/testFibonacci.java
@@ -11,5 +11,34 @@ class testFibonacci {
 		int valor = fibonacci.serieFibonacci(0);
         assertEquals(0, valor);
 	}
+	
+	@Test
+	void test1() {
+		int valor = fibonacci.serieFibonacci(1);
+        assertEquals(1, valor);
+	}
 
+	@Test
+	void test2() {
+		int valor = fibonacci.serieFibonacci(2);
+        assertEquals(1, valor);
+	}
+	
+	@Test
+	void test3() {
+		int valor = fibonacci.serieFibonacci(3);
+        assertEquals(2, valor);
+	}
+	
+	@Test
+	void test4() {
+		int valor = fibonacci.serieFibonacci(4);
+        assertEquals(3, valor);
+	}
+	
+	@Test
+	void test5() {
+		int valor = fibonacci.serieFibonacci(5);
+        assertEquals(5, valor);
+	}
 }
